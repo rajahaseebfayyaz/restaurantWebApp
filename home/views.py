c3cd9d9f1cff23012e08afee5201d68917ee0ded
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2023-06-24 23:32:16 +0530

    Created Home in app and created urls , apps , views

---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+# from meals.models import Meal , Category
+# from blog.models import Post
+# from aboutus.models import WhyChooseUs
+
+
+def home(request):
+
+    # meals = Meal.objects.all()
+    # meal_list = Meal.objects.all()
+    # categories = Category.objects.all()
+    # posts = Post.objects.all()
+    # latest_post = Post.objects.last()
+    # why_choose_us = WhyChooseUs.objects.all()
+
+
+
+    # context = {
+    #     'meals' : meals ,
+    #     'meal_list' : meal_list ,
+    #     'categories' : categories ,
+    #     'posts' : posts ,
+    #     'latest_post' : latest_post ,
+    #     'why_choose_us' : why_choose_us ,
+    # }
+    context = None
+
+    return render(request , 'index.html' , context)
--- restaurantWebApp/urls.py
+++ restaurantWebApp/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls.conf import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace='home')),
 ]
