56ed87068ffd12f1761261840ec88c4bb8446599
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2023-06-25 11:30:11 +0530

    Code refactoring and restructring + added in ternal logic and fix errors

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,6 @@ from . import views
 
 app_name = 'home'
 
-"""
-    @rajaHaseebFayyaz
-    urlpatterns will decide which sub page to be handled from the home page.
-"""
 urlpatterns = [
     path('',views.home , name='home' ),
 ]
--- home/views.py
+++ home/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from meals.models import Meal
+from meals.models import Meal , Category
+from blog.models import Post
+from aboutus.models import WhyChooseUs
 
 
 def home(request):
 
     meals = Meal.objects.all()
-    
-    """
-        @rajaHaseebFayyaz
-        Passing created index.html to be render on home page loading.
-    """
-    
+    meal_list = Meal.objects.all()
+    categories = Category.objects.all()
+    posts = Post.objects.all()
+    latest_post = Post.objects.last()
+    why_choose_us = WhyChooseUs.objects.all()
+
+
 
     context = {
-        'meals' : meals
+        'meals' : meals ,
+        'meal_list' : meal_list ,
+        'categories' : categories ,
+        'posts' : posts ,
+        'latest_post' : latest_post ,
+        'why_choose_us' : why_choose_us ,
     }
 
     return render(request , 'index.html' , context)
